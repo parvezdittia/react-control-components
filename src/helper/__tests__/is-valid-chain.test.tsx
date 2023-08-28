9d937046e7593cdcae51e277593b575d2b0e4023
parvezdittia <53978217+parvezdittia@users.noreply.github.com>
2023-08-28 19:29:34 +0000

    is-valid-chain helper function

---
+++ src/helper/__tests__/is-valid-chain.test.tsx
@@ -0,0 +1,100 @@
+import { Else, ElseIf, If } from '../../components';
+import { isValidChain } from '../is-valid-chain';
+import React from 'react';
+
+
+describe('isValidChain', () => {
+  it('should return true if chain is valid', () => {
+    const result = isValidChain([
+      <If condition={true}>
+        <span>1</span>
+      </If>,
+      <Else>
+        <span>2</span>
+      </Else>,
+    ]);
+
+    expect(result).toBe(true);
+  });
+
+  it('should return false if the first child is not an If', () => {
+    const result = isValidChain([
+      <span>1</span>,
+      <Else>
+        <span>2</span>
+      </Else>,
+    ]);
+
+    expect(result).toBe(false);
+  });
+
+  it('should return false if the last child is not an Else', () => {
+    const result = isValidChain([
+      <If condition={true}>
+        <span>1</span>
+      </If>,
+      <span>2</span>,
+    ]);
+
+    expect(result).toBe(false);
+  });
+
+  it('should return false if a child between the first and last is not an ElseIf', () => {
+    const result = isValidChain([
+      <If condition={true}>
+        <span>1</span>
+      </If>,
+      <span>2</span>,
+      <Else>
+        <span>3</span>
+      </Else>,
+    ]);
+
+    expect(result).toBe(false);
+  });
+
+  it('should return false if the chain is empty', () => {
+    const result = isValidChain([]);
+    expect(result).toBe(false);
+  });
+
+  it('should return true if the chain has only one child and that child is <If />', () => {
+    const result = isValidChain([
+      <If condition={true}>
+        <span>1</span>
+      </If>,
+    ]);
+
+    expect(result).toBe(true);
+  });
+
+  it('should return false if the chain has only one child and that child is not <If />', () => {
+    const result = isValidChain([<ElseIf condition={true}>1</ElseIf>]);
+
+    expect(result).toBe(false);
+  });
+
+  it('should return true of the chain has only two children and the first is <If /> and the second is <Else />', () => {
+    const result = isValidChain([
+      <If condition={true}>
+        <span>1</span>
+      </If>,
+      <Else>
+        <span>2</span>
+      </Else>,
+    ]);
+
+    expect(result).toBe(true);
+  });
+
+  it('should return true if the chain has only two children and the first is <If /> and the second is <ElseIf />', () => {
+    const result = isValidChain([
+      <If condition={true}>
+        <span>1</span>
+      </If>,
+      <ElseIf condition={true}>2</ElseIf>,
+    ]);
+
+    expect(result).toBe(true);
+  });
+});
---
+++ src/helper/is-valid-chain.ts
@@ -0,0 +1,46 @@
+import { Children, ReactNode, isValidElement } from 'react';
+import { Else, ElseIf, If } from '../components';
+
+export const isValidChain = (children: ReactNode) => {
+  const childrenArray = Children.toArray(children);
+  const firstChild = childrenArray[0];
+  const lastChild = childrenArray[childrenArray.length - 1];
+
+  if (childrenArray.length === 0) {
+    return false;
+  }
+
+  if (childrenArray.length === 1) {
+    return isValidElement(firstChild) && firstChild.type === If;
+  }
+
+  if (
+    childrenArray.length === 2 &&
+    isValidElement(firstChild) &&
+    isValidElement(lastChild)
+  ) {
+    return (
+      firstChild.type === If &&
+      (lastChild.type === Else || lastChild.type === ElseIf)
+    );
+  }
+
+  if (
+    !isValidElement(firstChild) ||
+    firstChild.type !== If ||
+    !isValidElement(lastChild) ||
+    lastChild.type !== Else
+  ) {
+    return false;
+  }
+
+  for (let i = 1; i < childrenArray.length - 1; i++) {
+    const child = childrenArray[i];
+
+    if (!isValidElement(child) || child.type !== ElseIf) {
+      return false;
+    }
+  }
+
+  return true;
+};
